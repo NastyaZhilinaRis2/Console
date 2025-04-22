a7ddf31945ff403a4cff7c4faacf7d7afbe685b3
NastyaZhilinaRis2 <149530615+NastyaZhilinaRis2@users.noreply.github.com>
2025-04-22 19:05:39 +0500

    y

---
+++ жук пожиратель/жук пожиратель/жук пожиратель.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <stdlib.h> 
+#include <ctime>
+#include <conio.h>
+
+using namespace std;
+
+bool gameOver;
+const int width = 25;
+const int height = 20;
+int x, y, fruitX, fruitY, score = 0;
+enum eDirection { STOP, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+int flag;
+
+void Setup() {
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	srand(time(0));
+	fruitX = 2 + rand() % (width - 1 - 2 + 1);
+	fruitY = 2 + rand() % (height - 1 - 2 + 1);
+	score = 0;
+}
+void Draw() {
+	system("cls");
+	for (int i = 1; i <= width; i++)
+		cout << "%";
+	cout << endl;
+
+	for (int i = 2; i < height; i++)
+	{
+		for (int j = 1; j <= width; j++)
+		{
+			if (j == 1 || j == width)
+				cout << "%";
+			else
+				if (x == j && y == i)
+					cout << "O";
+				else if (fruitX == j && fruitY == i)
+					cout << "*";
+				else cout << " ";
+
+
+		}
+		cout << endl;
+	}
+
+	for (int i = 1; i <= width; i++)
+		cout << "%";
+	cout << endl;
+	cout << "\nscore = " << score;
+}
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 75:
+		case 228:
+		case 'a': dir = LEFT;  break;
+		case 77:
+		case 162:
+		case 'd': dir = RIGHT;  break;
+		case 72:
+		case 230:
+		case 'w': dir = UP;  break;
+		case 80:
+		case 235:
+		case 's': dir = DOWN; break;
+		}
+	}
+}
+void Logic()
+{
+	switch (dir)
+	{
+	case LEFT: x--; break;
+	case RIGHT: x++; break;
+	case UP: y--; break;
+	case DOWN: y++; break;
+	}
+	if (x == 1 || x == width || y == 1 || y == height)
+		gameOver = true;
+	if (x == fruitX && y == fruitY)
+	{
+		fruitX = 2 + rand() % (width - 1 - 2 + 1);
+		fruitY = 2 + rand() % (height - 1 - 2 + 1);
+		score++;
+	}
+}
+void Exit()
+{
+	int otstupX, otstupY;
+	bool m = true;
+	while (m == true)
+	{
+		system("cls");
+		for (otstupY = height / 2; otstupY >= 0; otstupY-=2) cout << "\n"; 
+		for (otstupX = width / 2; otstupX >= 0; otstupX -= 2) cout << " ";
+		cout << "Game Over!\n";
+		for (otstupX = width / 2; otstupX >= 0; otstupX -= 2) cout << " ";
+		cout << "score = " << score << "\n";
+		for (otstupX = width / 2; otstupX >= 0; otstupX -= 2) cout << " ";
+		cout << "Чтобы начать заново, нажмите 1\n";
+		for (otstupX = width / 2; otstupX >= 0; otstupX -= 2) cout << " ";
+		cout << "Чтобы выйти, нажмите ESC";
+		for (otstupY = height / 2; otstupY >= 0; otstupY -= 2) cout << "\n";
+		for (otstupX = width / 2; otstupX >= 0; otstupX -= 2) cout << " ";
+		cout <<"\n";
+		if (_kbhit())
+		{
+			switch (_getch())
+			{
+			case 27: m = false; gameOver = true; break;
+			case 49: m = false; flag = 1; break;
+			}
+		}
+	}
+}
+int main()
+{
+	repeat:
+	setlocale(LC_ALL, "RUSSIAN");
+	Setup();
+	while (gameOver == false)
+	{
+		Draw();
+		Input();
+		Logic();
+	}
+	Exit();
+	if (flag == 1) { goto repeat; };
+}
---
+++ кол-во до НГ консоль/кол-во до НГ/кол-во до НГ.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month, day, ostalos, ostalos_v_mes, ostalos_posle = 0, colvo_dney, fev, ostalos1 = 0;
+
+    setlocale(LC_ALL, "RUSSIAN");
+    cout << "Введите номер месяца: ";
+    cin >> month;
+
+    cout << "Введите день: ";
+    cin >> day;
+
+        if (month > 12 || month == 0 || day > 31 || day == 0 || month == 4 && day > 30 || month == 6 && day > 30 || month == 9 && day > 30 || month == 11 && day > 30 || month == 2 && day > 29) cout << "Такой даты не существует.\n";
+        else
+        {
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) colvo_dney = 31;
+            if (month == 1 || month == 2)
+            {
+                cout << "Сколько дней в феврале? ";
+                cin >> fev;
+                if (month == 2) colvo_dney = fev;
+            }
+            else colvo_dney = 30;
+            if ((month == 2 && fev > 29) || (month == 2 && fev < 28) || (month == 2 && day > fev)) cout << "Такой даты не существует.\n";
+            else 
+            {
+                ostalos_v_mes = colvo_dney - day + 1;
+
+                if (month == 1) ostalos_posle = fev + 6 * 31 + 30 * 4;
+                if (month == 2) ostalos_posle = 6 * 31 + 30 * 4;
+                if (month == 3) ostalos_posle = 5 * 31 + 30 * 4;
+                if (month == 4) ostalos_posle = 5 * 31 + 30 * 3;
+                if (month == 5) ostalos_posle = 4 * 31 + 30 * 3;
+                if (month == 6) ostalos_posle = 4 * 31 + 30 * 2;
+                if (month == 7) ostalos_posle = 3 * 31 + 30 * 2;
+                if (month == 8) ostalos_posle = 2 * 31 + 30 * 2;
+                if (month == 9) ostalos_posle = 2 * 31 + 30;
+                if (month == 10) ostalos_posle = 31 + 30;
+                if (month == 11) ostalos_posle = 31;
+
+                ostalos = ostalos_v_mes + ostalos_posle;
+
+                ostalos1 = ostalos % 10;
+
+                    if (ostalos1 == 1 && ostalos != 11)
+                        cout << "До Нового года осталось: " << ostalos << " день.\n";
+                    else
+                        if ((ostalos1 == 2 || ostalos1 == 3 || ostalos1 == 4 ) && ostalos != 12 && ostalos != 13 && ostalos != 14)
+                            cout << "До Нового года осталось: " << ostalos << " дня.\n";
+                        else cout << "До Нового года осталось: " << ostalos << " дней.\n";       
+            }
+        }
+    system("pause");
+}
